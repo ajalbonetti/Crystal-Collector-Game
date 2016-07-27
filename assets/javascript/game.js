93823440913a03ed47ad855cf67b1b1e37484068
AA <AA@AAs-iMac-2.local>
2016-07-27 07:55:49 -0700

    moved crystals into function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,74 +32,50 @@ function getrandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function checkScore() {
+	if (totalScore == randomNumber) {
+		wins++;
+		$("#wins").html(wins);
+		$("#message").html("You Win!");
+		gameStart();
+		} else if (totalScore > randomNumber) {
+			losses++;
+			$("#losses").html(losses);
+			$("#message").html("You Lose!");
+			gameStart();	
+		}
+}
+
+
 // Crystal Click Functions
 
 $("#pink").click(function() {
 	totalScore += pinkCrystal;
 	$("#totalScore").html(totalScore);
 	$("#message").html("");
-		if (totalScore == randomNumber) {
-		wins++;
-		$("#wins").html(wins);
-		$("#message").html("You Win!");
-		gameStart();
-		} else if (totalScore > randomNumber) {
-			losses++;
-			$("#losses").html(losses);
-			$("#message").html("You Lose!");
-			gameStart();	
-		}
+	checkScore();
 });
 
 $("#green").click(function() {
 	totalScore += greenCrystal;
 	$("#totalScore").html(totalScore);
 	$("#message").html("");
-		if (totalScore == randomNumber) {
-		wins++;
-		$("#wins").html(wins);
-		$("#message").html("You Win!");
-		gameStart();
-		} else if (totalScore > randomNumber) {
-			losses++;
-			$("#losses").html(losses);
-			$("#message").html("You Lose!");
-			gameStart();	
-		}
+	checkScore();	
+	
 });
 
 $("#blue").click(function() {
 	totalScore += blueCrystal;
 	$("#totalScore").html(totalScore);
 	$("#message").html("");
-		if (totalScore == randomNumber) {
-		wins++;
-		$("#wins").html(wins);
-		$("#message").html("You Win!");
-		gameStart();
-		} else if (totalScore > randomNumber) {
-			losses++;
-			$("#losses").html(losses);
-			$("#message").html("You Lose!");
-			gameStart();	
-		}
+	checkScore();
 });
 
 $("#orange").click(function() {
 	totalScore += orangeCrystal;
 	$("#totalScore").html(totalScore);
 	$("#message").html("");
-		if (totalScore == randomNumber) {
-		wins++;
-		$("#wins").html(wins);
-		$("#message").html("You Win!");
-		gameStart();
-		} else if (totalScore > randomNumber) {
-			losses++;
-			$("#losses").html(losses);
-			$("#message").html("You Lose!");
-			gameStart();	
-		}
+	checkScore();
 });
 
 
